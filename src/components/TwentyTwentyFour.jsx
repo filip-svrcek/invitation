93a55d4a9f98cfc35833c75b09f8af1322e19dbe
Filip Svrcek <svrcek@cngroup.dk>
2024-07-11 21:32:09 +0200

    string adjustment

--- src/components/TwentyTwentyFour.jsx
+++ src/components/TwentyTwentyFour.jsx
@@ -4,8 +4,8 @@ export const TwentyTwentyFour = () => {
   return (
     <>
       <Fireworks autorun={{ speed: 1.5 }} />
-      <h2>Oběd restaurace Tom, Zlín</h2>
-      <h2>20.7. 11:30</h2>
+      <h2>Oběd v restauraci Tom</h2>
+      <h2>Zlín, 20.7. 11:30</h2>
       <h2>...a poté posezení v Malé ulici!</h2>
     </>
   );
