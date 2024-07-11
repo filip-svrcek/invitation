6ff2cdac8e851b70fc87031129910c6b9a685617
Filip Svrcek <svrcek@cngroup.dk>
2024-07-11 21:31:13 +0200

    fireworks intensity

--- src/components/TwentyTwentyFour.jsx
+++ src/components/TwentyTwentyFour.jsx
@@ -3,7 +3,7 @@ import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 export const TwentyTwentyFour = () => {
   return (
     <>
-      <Fireworks autorun={{ speed: 0.5 }} />
+      <Fireworks autorun={{ speed: 1.5 }} />
       <h2>Oběd restaurace Tom, Zlín</h2>
       <h2>20.7. 11:30</h2>
       <h2>...a poté posezení v Malé ulici!</h2>
